264f7044c17dab4ccbb302cb580f9391fc516782
Aditya Aggarwal <adi.adag@gmail.com>
2018-04-11 01:12:07 +0530

    making fetch request

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+	constructor(props)
+	{
+		super(props);
+
+		this.state={
+			paras: 4,
+			html: true,
+			text: ' '
+		}
+	}
+
+	componentWillMount()
+	{
+		this.getSampleText();
+	}
+
+	getSampleText()
+	{
+		const hipsterURL="http://hipsterjesus.com/api/";
+		fetch(hipsterURL+"?paras="+this.state.paras+"&html="+this.state.html)
+		.then(function(res){
+			return res.json();
+		})
+		.then(function(data){
+			let stringJSON=JSON.stringify(data);
+			console.log(stringJSON);
+		})
+	}
+
+  render() 
+  {
     return (
       <div className="App">
         Hello
